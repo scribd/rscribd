d48856488c1aa4168e9070cffe6229681b67658b
Tim Morgan <riscfuture@gmail.com>
2008-11-26 20:01:07 -0800

    Error spec

---
+++ spec/error_spec.rb
@@ -0,0 +1,11 @@
+old_dir = Dir.getwd
+Dir.chdir(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rscribd'
+
+describe Scribd::ResponseError do  it "should set the code attribute on initialization" do
+    error = Scribd::ResponseError.new(123)
+    error.code.should eql(123)
+  end
+end
+
+Dir.chdir old_dir
