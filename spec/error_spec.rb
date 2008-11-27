78e07ff1902cb65919a3679bec0080e869a37bbd
Tim Morgan <riscfuture@gmail.com>
2008-11-26 20:02:04 -0800

    Spacing fix

--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -2,7 +2,8 @@ old_dir = Dir.getwd
 Dir.chdir(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rscribd'
 
-describe Scribd::ResponseError do  it "should set the code attribute on initialization" do
+describe Scribd::ResponseError do
+  it "should set the code attribute on initialization" do
     error = Scribd::ResponseError.new(123)
     error.code.should eql(123)
   end
