c32cd0525eb991b56a3dfd0b5f7f304c6bf063aa
Tim Morgan <tim@scribd.com>
2010-05-11 22:58:32 -0700

    Reorganize files

--- lib/rscribd.rb
+++ lib/rscribd.rb
@@ -1,38 +1,14 @@
 #!/usr/bin/env ruby
 
-module Scribd # :nodoc:
+# Container module for all classes in the RScribd gem.
+
+module Scribd
 end
 
-class Symbol
-  def to_proc
-    Proc.new { |*args| args.shift.__send__(self, *args) }
-  end unless method_defined?(:to_proc)
-end
-
-class Hash #:nodoc:
-   # Taken from Rails, with appreciation to DHH
-   def stringify_keys
-     inject({}) do |options, (key, value)|
-       options[key.to_s] = value
-       options
-     end
-   end unless method_defined?(:stringify_keys)
-end
-
-class Array #:nodoc:
-  def to_hsh
-    h = Hash.new
-    each { |k, v| h[k] = v }
-    h
-  end
-end
-
-# The reason these files have such terrible names is so they don't conflict with
-# files in Rails's app/models directory; Rails seems to prefer loading those
-# when require is called.
-require 'scribdmultiparthack'
-require 'scribderrors'
-require 'scribdapi'
-require 'scribdresource'
-require 'scribddoc'
-require 'scribduser'
+require 'support/extensions'
+require 'support/multipart_hack'
+require 'scribd/errors'
+require 'scribd/api'
+require 'scribd/resource'
+require 'scribd/document'
+require 'scribd/user'
---
+++ lib/support/extensions.rb
@@ -0,0 +1,23 @@
+class Symbol
+  def to_proc
+    Proc.new { |*args| args.shift.__send__(self, *args) }
+  end unless method_defined?(:to_proc)
+end
+
+class Hash #:nodoc:
+   # Taken from Rails, with appreciation to DHH
+   def stringify_keys
+     inject({}) do |options, (key, value)|
+       options[key.to_s] = value
+       options
+     end
+   end unless method_defined?(:stringify_keys)
+end
+
+class Array #:nodoc:
+  def to_hsh
+    h = Hash.new
+    each { |k, v| h[k] = v }
+    h
+  end
+end
